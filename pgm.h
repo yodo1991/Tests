510df120adbd65572933342ff6163d7e6162dee5
root <root@LAPTOP-USO28RA3.localdomain>
2023-03-21 13:46:30 +0800

    version2

--- pgm.h
+++ pgm.h
@@ -13,4 +13,15 @@ typedef struct _pgm {
 #define MAGIC_NUMBER_ASCII_PGM 0x3250
 #define MIN_IMAGE_DIMENSION 1
 #define MAX_IMAGE_DIMENSION 65536
-#define MAX_COMMENT_LINE_LENGTH 1288
+#define MAX_COMMENT_LINE_LENGTH 128
+
+#define Bad_Argument_Count 1 //Program given wrong # of arguments 
+#define Bad_File_Name 2 //Program fails to open file 
+#define Bad_Magic_Number 3 //File has an illegal magic number 
+#define Bad_Comment_Line 4 //Program failed on comment line 
+#define Bad_Dimensions 5 //File has illegal dimensions 
+#define Bad_Max_Gray_Value 6 //Program failed on max gray value 
+#define Image_Malloc_Failed 7 // Malloc failed to allocate memory 
+#define Bad_Data 8 //Reading in data failed 
+#define Output_Failed 9 //Writing out data failed 
+#define Miscellaneous (text description) 100 // Any other error which is detected 
--- pgmEcho.c
+++ pgmEcho.c
@@ -4,11 +4,8 @@
 #include "pgm.h"
 #include "readData.h"
 #include "writeData.h"
+#include "pgmError.h"
 
-#define EXIT_NO_ERRORS 0
-#define EXIT_WRONG_ARG_COUNT 1
-#define EXIT_BAD_INPUT_FILE 2
-#define EXIT_BAD_OUTPUT_FILE 3
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -23,11 +20,12 @@ int main(int argc, char **argv) {
 		/* print an error message        */
 		printf("Usage: %s inputImage.pgm outputImage.pgm\n", argv[0]);
 		/* and return an error code      */
-		return EXIT_WRONG_ARG_COUNT;
+		ExitWithError(Bad_Argument_Count, NULL);// 1 means 'Bad Argument Count'
 	} /* wrong arg count */
 	
 
 	char *r_filename=argv[1];
+	
 	char *w_filename=argv[2];
 	pgm =read_data(pgm, r_filename);
 	write_data(pgm, w_filename);
